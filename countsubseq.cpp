4d5be003d2b9e43b00ae66ee2a355a78b4b72cc3
Sharath Chandra.D <65011823+sharath315@users.noreply.github.com>
2022-04-23 17:57:23 +0530

    Add files via upload

---
+++ countsubseq.cpp
@@ -0,0 +1,27 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int countsubseq(int i,vector<int>&arr,vector<int>&ds,int k,int sum){
+    if(i==arr.size()){
+        if(sum==k)
+            return 1;
+        return 0;
+    }
+    //pick
+    sum+=arr[i];
+    ds.push_back(arr[i]);
+    int l=countsubseq(i+1,arr,ds,k,sum);
+    //not pick
+    sum-=arr[i];
+    ds.pop_back();
+    int r=countsubseq(i+1,arr,ds,k,sum);
+    return l+r;
+}
+
+int main(){
+    vector<int>arr={1,2,1,3,-1,2};
+    int k=2;
+    vector<int>ds;
+    cout<<"Number of subsequence with given sum is:"<<countsubseq(0,arr,ds,k,0);
+}
